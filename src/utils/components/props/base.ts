ee806f9de0213af5e2eae03744b105d607aa6086
Nico2433 <nicobrab243@gmail.com>
2024-06-05 22:59:59 -0300

    fix: fixed visible className

--- src/utils/components/props/base.ts
+++ src/utils/components/props/base.ts
@@ -8,7 +8,7 @@ export const getBaseProps = <T extends GuiObject>({
 }: ReactComponent<T>): Partial<React.InstanceProps<T> | { [key: string]: unknown }> => {
 	return {
 		ref: forwardRef,
-		Visible: className.match("hidden")[0] ? true : false,
+		Visible: className.match("hidden")[0] ? false : true,
 		AnchorPoint: getAnchorValues(className),
 		Size: getSizeValues(className),
 	};
